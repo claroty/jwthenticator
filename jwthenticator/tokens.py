b08c971a5e307e5235a98c6f9d4666601c268dec
Guy Zylberberg <guyzyl@gmail.com>
2021-02-25 12:17:16 +0200

    Update tokens.py
    Fixed linting issue

--- jwthenticator/tokens.py
+++ jwthenticator/tokens.py
@@ -19,6 +19,7 @@ class TokenManager:
     Class responsible for the creation and loading of tokens
     """
 
+    # pylint: disable=too-many-arguments,too-many-instance-attributes
     def __init__(self, public_key: str, private_key: Optional[str] = None, algorithm: str = JWT_ALGORITHM,
                  jwt_lease_time: int = JWT_LEASE_TIME, jwt_audience: Optional[str] = JWT_AUDIENCE):
         """
