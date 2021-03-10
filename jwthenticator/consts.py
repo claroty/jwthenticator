136daa20ff52d87b7177f5a0edec467483ccfae9
Guy Zylberberg <guyzyl@gmail.com>
2021-03-10 15:41:48 +0200

    Converted consts to use environs package
    Added usage of the environs package which better parses environment variables

--- jwthenticator/consts.py
+++ jwthenticator/consts.py
@@ -1,39 +1,42 @@
 from __future__ import absolute_import
 
-from os import environ
+from environs import Env
 
 DAYS_TO_SECONDS = lambda x: x * 60 * 24
 
+env = Env()
+
 
 # Server consts
-PORT = int(environ.get("PORT", 8080))
-URL_PREFIX = URL_PREFIX = environ.get('URL_PREFIX', '')
-DISABLE_EXTERNAL_API = bool(environ.get("DISABLE_EXTERNAL_API", ""))
-DISABLE_INTERNAL_API = bool(environ.get("DISABLE_INTERNAL_API", ""))
+PORT = env.int("PORT", 8080)
+URL_PREFIX = URL_PREFIX = env.str("URL_PREFIX", "")
+DISABLE_EXTERNAL_API = env.bool("DISABLE_EXTERNAL_API", False)
+DISABLE_INTERNAL_API = env.bool("DISABLE_INTERNAL_API", False)
 
 # JWT consts
-JWT_ALGORITHM = environ.get("JWT_ALGORITHM", "RS256")
-JWT_ALGORITHM_FAMILY = environ.get("JWT_ALGORITHM_FAMILY", "RSA")
-JWT_LEASE_TIME = int(environ.get("JWT_LEASE_TIME", 30 * 60)) # In seconds - 30 minutes
-RSA_KEY_STRENGTH = int(environ.get("RSA_KEY_STRENGTH", 2048))
-JWT_AUDIENCE = environ.get("JWT_AUDIENCE", None)
+JWT_ALGORITHM = env.str("JWT_ALGORITHM", "RS256")
+JWT_ALGORITHM_FAMILY = env.str("JWT_ALGORITHM_FAMILY", "RSA")
+JWT_LEASE_TIME = env.int("JWT_LEASE_TIME", 30 * 60) # In seconds - 30 minutes
+JWT_AUDIENCE = env("JWT_AUDIENCE", None)
 
 # Token consts
-KEY_EXPIRY = int(environ.get("KEY_EXPIRY", DAYS_TO_SECONDS(120)))  # In seconds
-REFRESH_TOKEN_EXPIRY = int(environ.get("REFRESH_TOKEN_EXPIRY", DAYS_TO_SECONDS(60)))   # In seconds
+KEY_EXPIRY = env.int("KEY_EXPIRY", DAYS_TO_SECONDS(120))  # In seconds
+REFRESH_TOKEN_EXPIRY = env.int("REFRESH_TOKEN_EXPIRY", DAYS_TO_SECONDS(60))   # In seconds
 
-# Encryption keys
-RSA_PUBLIC_KEY = environ.get("RSA_PUBLIC_KEY", None)
-RSA_PRIVATE_KEY = environ.get("RSA_PRIVATE_KEY", None)
+# Keys from env
+RSA_PUBLIC_KEY = env("RSA_PUBLIC_KEY", None)
+RSA_PRIVATE_KEY = env("RSA_PRIVATE_KEY", None)
 # Keys from file
-RSA_PUBLIC_KEY_PATH = environ.get("RSA_PUBLIC_KEY_PATH", None)
-RSA_PRIVATE_KEY_PATH = environ.get("RSA_PRIVATE_KEY_PATH", None)
+RSA_PUBLIC_KEY_PATH = env("RSA_PUBLIC_KEY_PATH", None)
+RSA_PRIVATE_KEY_PATH = env("RSA_PRIVATE_KEY_PATH", None)
+# If no key is given, used for key generation
+RSA_KEY_STRENGTH = env.int("RSA_KEY_SIZE", 2048)
 
 # DB consts
-DB_CONNECTOR = environ.get("DB_CONNECTOR", "postgresql+pg8000")
-DB_USER = environ.get("DB_USER", "postgres")
-DB_PASS = environ.get("DB_PASS", "")
-DB_HOST = environ.get("DB_HOST", "localhost")
-DB_NAME = environ.get("DB_NAME", "jwthenticator")
+DB_CONNECTOR = env.str("DB_CONNECTOR", "postgresql+pg8000")
+DB_USER = env.str("DB_USER", "postgres")
+DB_PASS = env.str("DB_PASS", "")
+DB_HOST = env.str("DB_HOST", "localhost")
+DB_NAME = env.str("DB_NAME", "jwthenticator")
 
-DB_URI = environ.get("DB_URI", f"{DB_CONNECTOR}://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}")
+DB_URI = env.str("DB_URI", f"{DB_CONNECTOR}://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}")
