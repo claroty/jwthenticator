b35f167c06ea7db8e20a2e57f997496ac7443009
Pete Smiddy <peter.smiddy@libertymutual.com>
2022-02-09 16:44:39 -0500

    docker compose file changed so postgres container uses POSTGRES_HOST_AUTH_METHOD=trust and POSTGRES_DB=jwthenticator- and set environment variables for DB_NAME and DB_HOST in jwthenticator container- and removed DB environment variables from consts.py- THIS IS FOR LOCAL DEVELOPMENT ONLY

--- jwthenticator/consts.py
+++ jwthenticator/consts.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import
-
 from environs import Env
+
 
 DAYS_TO_SECONDS = lambda x: x * 60 * 24
 
 env = Env()
 
+# This consts.py file is for LOCAL development on a single-user workstation ONLY
 
 # Server consts
 PORT = env.int("PORT", 8080)
@@ -16,12 +17,14 @@ DISABLE_INTERNAL_API = env.bool("DISABLE_INTERNAL_API", False)
 # JWT consts
 JWT_ALGORITHM = env.str("JWT_ALGORITHM", "RS256")
 JWT_ALGORITHM_FAMILY = env.str("JWT_ALGORITHM_FAMILY", "RSA")
-JWT_LEASE_TIME = env.int("JWT_LEASE_TIME", 30 * 60) # In seconds - 30 minutes
+JWT_LEASE_TIME = env.int("JWT_LEASE_TIME", 30 * 60)  # In seconds - 30 minutes
 JWT_AUDIENCE = env.list("JWT_AUDIENCE", [])
 
 # Token consts
 KEY_EXPIRY = env.int("KEY_EXPIRY", DAYS_TO_SECONDS(120))  # In seconds
-REFRESH_TOKEN_EXPIRY = env.int("REFRESH_TOKEN_EXPIRY", DAYS_TO_SECONDS(60))   # In seconds
+REFRESH_TOKEN_EXPIRY = env.int(
+    "REFRESH_TOKEN_EXPIRY", DAYS_TO_SECONDS(60)
+)  # In seconds
 
 # Keys from env
 RSA_PUBLIC_KEY = env("RSA_PUBLIC_KEY", None)
@@ -31,12 +34,3 @@ RSA_PUBLIC_KEY_PATH = env("RSA_PUBLIC_KEY_PATH", None)
 RSA_PRIVATE_KEY_PATH = env("RSA_PRIVATE_KEY_PATH", None)
 # If no key is given, used for key generation
 RSA_KEY_STRENGTH = env.int("RSA_KEY_SIZE", 2048)
-
-# DB consts
-DB_CONNECTOR = env.str("DB_CONNECTOR", "postgresql+pg8000")
-DB_USER = env.str("DB_USER", "postgres")
-DB_PASS = env.str("DB_PASS", "")
-DB_HOST = env.str("DB_HOST", "localhost")
-DB_NAME = env.str("DB_NAME", "jwthenticator")
-
-DB_URI = env.str("DB_URI", f"{DB_CONNECTOR}://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}")
