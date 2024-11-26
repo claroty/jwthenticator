27ecd52ab8008bdc0cc6349bd737ff09bfac7898
Reuven Starodubski <reuven.s@claroty.com>
2024-11-26 10:08:26 +0200

    add test

--- jwthenticator/tests/test_tokens.py
+++ jwthenticator/tests/test_tokens.py
@@ -1,15 +1,33 @@
 from __future__ import absolute_import
 
+import time
+import os
+from datetime import timedelta
+from collections.abc import Callable, Generator
 from uuid import uuid4
 
 import pytest
 
 from jwthenticator.tokens import TokenManager
 from jwthenticator.keys import KeyManager
-from jwthenticator.utils import create_rsa_key_pair
+from jwthenticator.utils import create_rsa_key_pair, utcnow
 from jwthenticator.schemas import RefreshTokenData
 from jwthenticator.tests.utils import random_key, hash_key
 
+
+@pytest.fixture
+def set_timezone()-> Generator[Callable[[str], None], None, None]:
+    original_tz = os.environ.get('TZ')
+    def change_timezone(tz: str) -> None:
+        os.environ['TZ'] = tz
+        time.tzset()  # Update the timezone for the process
+    yield change_timezone
+    # Restore the original timezone
+    if original_tz is not None:
+        os.environ['TZ'] = original_tz
+    else:
+        del os.environ['TZ']
+    time.tzset()
 
 class TestTokens:
 
@@ -42,12 +60,17 @@ class TestTokens:
 
     # Create access token tests
     @pytest.mark.asyncio
-    async def test_create_access_token(self) -> None:
+    async def test_create_access_token(self, set_timezone: Callable[[str], None]) -> None:
         uuid = uuid4()
+        time_now = utcnow()
+        time_now_timestamp = int(time_now.timestamp())
+        time_plus_some_time = time_now + timedelta(seconds=10)
+        time_plus_some_time_timestamp = int(time_plus_some_time.timestamp())
+        set_timezone("America/Los_Angeles")
         token = await self.token_manager.create_access_token(uuid)
         token_data = await self.token_manager.load_access_token(token)
         assert token_data.identifier == uuid
-
+        assert time_now_timestamp <= token_data.iat <= time_plus_some_time_timestamp
 
     # Create refresh token tests
     @pytest.mark.asyncio
